3fd2c76100a398b86057218336e0e82be8376a35
romulotirinabejar-blip <romulotirinabejar@gmail.com>
2026-09-09 07:27:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,7 @@ if not api_key:
 
 # Configurar el modelo con tu clave
 genai.configure(api_key=api_key)
-model = genai.GenerativeModel("gemini-1.5-flash")
-
+model = genai.GenerativeModel("gemini-1.5-flash-latest")
 # Inicializar el historial de conversación en la sesión
 if "messages" not in st.session_state:
     st.session_state.messages = []
